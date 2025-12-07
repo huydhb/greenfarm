7ed1f6588085602b353aea84a9eec405eedf0163
huydhb <danghoangbaohuyc5@gmail.com>
2025-12-07 23:22:41 +0700

    chore: fix GitHub Pages deployment config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/greenfarm',
+  assetPrefix: '/greenfarm/',
 };
 
 export default nextConfig;
